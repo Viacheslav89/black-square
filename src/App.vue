563727eb458a16dc972e848378822d809b897612
To Do List <2113338@mail.ru>
2025-03-07 17:31:36 +0300

    create black square

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <AppBlackSquare />
+</template>
+
+
+<script setup lang="ts">
+import AppBlackSquare from './components/AppBlackSquare.vue';
+
+
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ src/components/AppBlackSquare.vue
@@ -0,0 +1,21 @@
+<template>
+  <main class="main">
+    <div class="square"></div>
+  </main>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+.square {
+  width: 300px;
+  height: 300px;
+  background-color: black;
+}
+</style>
